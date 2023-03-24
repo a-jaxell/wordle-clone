9b1878c4c9cdc9414939b1db87f26efba32e58c8
a-jaxell <97404672+a-jaxell@users.noreply.github.com>
2023-03-24 17:30:06 +0100

    Add findWord functionality

--- src/findWord.js
+++ src/findWord.js
@@ -1,18 +1,30 @@
 // input, en lista med ord samt en siffra som anger önskad längd, samt en indikation på om unika bokstäver är tilllåtna eller ej
-export function findWord(wordList, wordLength, multiChars) {
-  console.log(wordList.filter((e) => e.length === wordLength));
-}
-export const hasMultipleChars = (word) => {
-    const result = [];
+export function findWord(wordList, wordLength = 5, multiChars) {
     
-    // Iterates and checks if word[i] appears in the rest of the word from word[i +1]
-    for (let i = 0; i < word.length; i++){
-      const string = word.slice(i + 1, word.length);
-      result.push(string.includes(`${word[i]}`));
+  return multiChars === true
+    ? randomizeWord(wordList.filter((e) => e.length === wordLength))
+    : randomizeWord(
+        wordList
+          .filter((e) => !hasMultipleChars(e))
+          .filter((e) => e.length === wordLength)
+      );
     }
-    // Returns true in case any letter is included more than once in result
-    return result.includes(true);
-  };
+ 
+export const randomizeWord = (array) => {
+  return array[Math.floor(Math.random() * array.length)];
+};
+
+export const hasMultipleChars = (word) => {
+  const result = [];
+
+  // Iterates and checks if word[i] appears in the rest of the word from word[i +1]
+  for (let i = 0; i < word.length; i++) {
+    const string = word.slice(i + 1, word.length);
+    result.push(string.includes(`${word[i]}`));
+  }
+  // Returns true in case any letter is included more than once in result
+  return result.includes(true);
+};
 // Hantera på något väldefinierat sätt när inget passande ord finns
 
 // output, det slumpmässigt valda ordet
--- test/findWord.spec.js
+++ test/findWord.spec.js
@@ -1,9 +1,32 @@
-import { findWord, hasMultipleChars } from '../src/findWord.js';
+import { findWord, hasMultipleChars, randomizeWord } from '../src/findWord.js';
 import { describe, expect } from "@jest/globals";
 
-const mockArray = ['apple', 'banana', 'cherry', 'orange', 'grape', 'lemon', 'lime', 'kiwi', 'peach', 'pear'];
+const randNum = () =>  Math.floor(Math.random()*6);
 
+const mockArray = ['apple', 'banana', 'cherry', 'orange', 'grape', 'lemon', 'lime', 'kiwi', 'peach', 'pear'];
+/**
+ * Test Strategy:
+ * 
+ * In order to cover a
+ */
 describe("Class findWord", () =>{
+
+    test("Handles no matching words", () => {
+        expect(findWord(mockArray, 10, true)).toBe(
+            "There isnt a matching word for 10 characters"
+        )
+    })
+
+    test("Randomizes a word, returns a string greater than 0", () => {
+        expect(typeof randomizeWord(mockArray)).toBe("string");
+        expect(randomizeWord(mockArray).length).toBeGreaterThan(0);
+    })
+    test("Returns a string and of the correct length when such a parameter is supplied. Default is 5 characters long", () => {
+        // The word returned should be a string and the same length as the length submitted.
+        expect(typeof findWord(mockArray)).toBe("string");
+        expect(findWord(mockArray).length).toEqual(5);
+        expect(findWord(mockArray, 6).length).toEqual(6);
+    })
 
     test('Returns true/false depending on if a word has multiple characters or not.', () => {
         // 'orange' should return false
@@ -15,13 +38,13 @@ describe("Class findWord", () =>{
             true
         )
     });
-    test('Accept the correct format, else return error message.', () => {
-        expect(findWord(mockArray, 5, M)).toBe(
-            {word: returnedWord, length: 5, multiChars: true}
-        )
-        expect(findWord('Mustard', a, true)).toBe(
-            "Incorrect Input in findWord."
-        )
-    });
+    // test('Accept the correct format, else return error message.', () => {
+    //     expect(findWord(mockArray, 5, M)).toBe(
+    //         {word: returnedWord, length: 5, multiChars: true}
+    //     )
+    //     expect(findWord('Mustard', a, true)).toBe(
+    //         "Incorrect Input in findWord."
+    //     )
+    // });
 
 });
